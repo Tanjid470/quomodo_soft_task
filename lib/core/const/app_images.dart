d522e055d2365064bcf53223a006acf6dfba53ad
Tanjid1470 <tanjid@tutorsplan.com>
2026-01-23 08:24:36 +0600

    [ADDED]: Add image asset constants

---
+++ lib/core/const/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages{
+  AppImages._();
+  static String homeIcon = "assets/icons/home.png";
+  static String messageIcon = "assets/icons/message.png";
+  static String orderIcon = "assets/icons/other.png";
+  static String profileIcon = "assets/icons/profile.png";
+}
